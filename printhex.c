4afa2b939943d6cb5b9041fce0d4fe0f095e925b
nkaminski <nashkaminski@comcast.net>
2017-03-29 19:12:34 -0500

    Fix makefile and HID Corporate 1000 parity routines

--- printhex.c
+++ printhex.c
@@ -17,8 +17,10 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <fcntl.h>
-void main(int argc, char **argv){
+#define GETBIT(arr,x) ((arr[(x)/8] >> ((x)%8)) & 0x01)
+int main(int argc, char **argv){
 	int fd;
+	int f=0;
 	uint8_t buf[8];
 	ssize_t size;
 	fd = open("/dev/prox", O_RDONLY);
@@ -34,36 +36,44 @@ void main(int argc, char **argv){
 	if(size==5){
 		uint16_t facility;
 		uint32_t cardnum;
-		uint8_t paritya=0,parityb=0,parityc=0,f=0;
+		uint8_t paritya=0,parityb=0,parityc=0;
 		int i;
 		printf("Attempting HID Corporate 1000 Decode (35 bits).....\n");
 		facility = (((uint16_t)buf[4] << 11) | ((uint16_t)buf[3] << 3) | (buf[2] >> 5)) & 0x0FFF;
 		cardnum = (((uint32_t)buf[2] << 15) | ((uint32_t)buf[1] << 7) | ((uint32_t)buf[0] >> 1)) & 0x000FFFFF;  
-		for(i=2;i<35;i++){
-			paritya += ((buf[i/8] >> (i % 8)) & 0x01);
-			i++;
-			paritya += ((buf[i/8] >> (i % 8)) & 0x01);
+		/* Parity scheme (using ONE indexing):
+		Even  2  3, 4, 6, 7, 9, 10,12,13,15,16,18,19,21,22,24,25,27,28,30,31,33,34
+		Odd  35 2, 3, 5, 6, 8, 9, 11,12,14,15,17,18,20,21,23,24,26,27,29,30,32,33
+		Odd  1  2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35
+		*/
+		paritya = GETBIT(buf,1);
+		for(i=2;i<34;i=i+3){
+			paritya ^= GETBIT(buf,i);
+			paritya ^= GETBIT(buf,i+1);
+			
 		}
 		if((paritya % 2) == 1){
 			f++;
+			fprintf(stderr,"Parity check 1 failed!\n");
 			goto fail;
-
 		}
-		for(i=1;i<35;i++){
-			parityb += ((buf[i/8] >> (i % 8)) & 0x01);
-			i++;
-			parityb += ((buf[i/8] >> (i % 8)) & 0x01);
+		parityb = GETBIT(buf,34);
+		for(i=1;i<34;i=i+3){
+			parityb ^= GETBIT(buf,i);
+			parityb ^= GETBIT(buf,i+1);
 		}
 		if((parityb % 2) == 0){
 			f++;
+			fprintf(stderr,"Parity check 2 failed!\n");
 			goto fail;
 
 		}
 		for(i=0;i<35;i++){
-			parityc += ((buf[i/8] >> (i % 8)) & 0x01);
+			parityc ^= GETBIT(buf,i);
 		}
 		if((parityc % 2) == 0){
-			f++;			
+			f++;
+			fprintf(stderr,"Parity check 3 failed!\n");	
 			goto fail;
 		}
 		printf("Facility Code: %i, Card Number %lu\n", facility, cardnum);
@@ -71,4 +81,9 @@ fail:		if(f)
 			printf("HID Corporate 1000 Decode Failed!\n");
 	}		
 	close(fd);
+	if(f){
+		return 1;
+	} else {
+		return 0;
+	}
 }
